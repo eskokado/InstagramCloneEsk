38e49d34ed04a27ac576e80d033436e9e8d2c06a
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-09 11:10:48 -0300

    Renderização condicional do login e do cadastro

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -35,6 +35,8 @@ export class AcessoComponent implements OnInit {
   public estadoBanner: string = 'criado'
   public estadoPainel: string = 'criado'
 
+  public cadastro: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
