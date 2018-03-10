a13f29cd13bce8006a466c9cb9581f333db83826
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-10 09:58:50 -0300

    Protegendo a rota home com CanActivate(checando autenticação) parte 2

--- src/app/autenticacao-guard.service.ts
+++ src/app/autenticacao-guard.service.ts
@@ -1,8 +1,16 @@
+import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { Autenticacao } from './autenticacao.service'
 
+@Injectable()
 export class AutenticacaoGuard implements CanActivate {
-  canActivate(): boolean {
-    return false
+
+  constructor(
+    private autenticacao: Autenticacao
+  ){}
+
+  canActivate(): boolean {    
+    return this.autenticacao.autenticado()
   }
 
 }
