cd89d4539677c90baf198dea1b4af4490a83e2ff
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-10 10:11:49 -0300

    Utilizando LocalStorage para armazenamento do token de autenticação

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -39,6 +39,7 @@ export class Autenticacao {
         firebase.auth().currentUser.getIdToken()
           .then((idToken: string) => {
             this.token_id = idToken
+            localStorage.setItem('idToken', idToken)
             this.router.navigate(['/home'])
           })
       })
@@ -46,6 +47,11 @@ export class Autenticacao {
   }
 
   public autenticado(): boolean {
+
+    if (this.token_id === undefined && localStorage.getItem('idToken') !== null) {
+      this.token_id = localStorage.getItem('idToken')
+    }
+
     return this.token_id !== undefined
   }
 
