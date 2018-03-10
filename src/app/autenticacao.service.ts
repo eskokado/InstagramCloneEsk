cb578185144fb8cb8276e83177f4bfb2b16e7576
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-10 09:23:46 -0300

    Armazenando o token de autenticação dentro do serviço Autenticação

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -2,6 +2,9 @@ import { Usuario } from "./acesso/usuario.model";
 import * as firebase from 'firebase';
 
 export class Autenticacao {
+
+  public token_id: string
+
   public cadastarUsuario(usuario: Usuario): Promise<any> {
     //console.log('Chegamos até o serviço: ', usuario)
 
@@ -26,7 +29,13 @@ export class Autenticacao {
     console.log('email:', email)
     console.log('senha:',senha)
     firebase.auth().signInWithEmailAndPassword(email, senha)
-      .then((resposta: any) => console.log(resposta))
+      .then((resposta: any) => {
+        firebase.auth().currentUser.getIdToken()
+          .then((idToken: string) => {
+            this.token_id = idToken
+            console.log(this.token_id)
+          })
+      })
       .catch((error: Error) => console.log(error))
   }
 
