cdf3b14a6db560e273a7f4310cf999f7e4492d4e
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-11 10:21:04 -0300

    Controlando o progresso do upload

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -2,7 +2,29 @@ import * as firebase from 'firebase';
 
 export class Bd {
   public publicar(publicacao: any) : void {
+    console.log(publicacao)
+
+    let nomeImagem = Date.now()
+
+    firebase.storage().ref()
+      .child(`imagens/${nomeImagem}`)
+      .put(publicacao.imagem)
+      .on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: any) => {
+          // acompanhamento de snapshot
+          console.log(snapshot)
+        },
+        (error) => {
+          console.log(error)
+        },
+        () => {
+          // finalização do processo
+          console.log('upload completo')
+        }
+      )
+    /*
   firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
     .push( { titulo: publicacao.titulo } )
+    */
   }
 }
--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -12,6 +12,7 @@ import { Bd } from '../../bd.service'
 export class IncluirPublicacaoComponent implements OnInit {
 
   public email: string
+  public imagem: any
 
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
@@ -30,11 +31,12 @@ export class IncluirPublicacaoComponent implements OnInit {
   public publicar(): void {
     this.bd.publicar({
       email: this.email,
-      titulo: this.formulario.value.titulo
+      titulo: this.formulario.value.titulo,
+      imagem: this.imagem[0]
     })
   }
 
   public preparaImagemUpload(event: Event): void {
-    console.log((<HTMLInputElement>event.target).files)
+    this.imagem = (<HTMLInputElement>event.target).files
   }
 }
